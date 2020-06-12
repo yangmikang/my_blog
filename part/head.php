88dbb06815a234312a70474da1755dabec62a1ce
yangmikang <ymk0668@naver.com>
2020-06-12 18:35:28 +0900

    상단바

---
+++ part/head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>블로그</title>
+    <link rel="stylesheet" href="/resource/common.css">
+    <script src="/resource/common.js"></script>
+</head>
+<body>
+    <div class="top-bar">
+        <div class="con height-100p flex">
+            <nav class="menu-box flex flex-1-0-0">
+                <ul class="flex flex-1-0-0">
+                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
+                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
+                    </li>
+                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
+                    </li>
+                    <li class="flex-1-0-0 flex">
+                        <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
+                    </li>
+            </nav>
+        </div>
+    </div> 
--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
-$dan = 0
-for ($i=1; $i<=9; $i++){
-    $rs = $dan *$i
-    echo "<div>($dan)*{$i}={$rs}</div>"
-}
+include "../part/head.php"
 ?>
-    
+<h1>안녕하세요</h1>
+<?php
+include "../part/foot.php"
+?>
