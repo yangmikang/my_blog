82f7e9ad309a2bd3428b252fc3bb1413512bfa16
yangmikang <ymk0668@naver.com>
2020-06-16 18:51:16 +0900

    모바일 사이드 메뉴 추가

--- public/resource/common.js
+++ public/resource/common.js
@@ -1,29 +1,37 @@
+function MobileSideBar__toggle() {
+    var $btn = $('.btn-toggle-mobile-side-bar');
+    var $btn2 = $('.mobile-side-bar');
+    var $btn3 = $('.background');
+    var $btn4 = $('html');
 
-
-function MobileSideBar__toggle(){
-        var $btn = $('.btn-toggle-mobile-side-bar');
-        var $btn2 = $('.mobile-side-bar');
-        var $btn3 =$('.background');
-        var $btn4 =$('html');
-
-        if($btn.hasClass('active')){
-            $btn.removeClass('active');
-            $btn2.removeClass('active');
-            $btn3.removeClass('active');
-            $btn4.removeClass('active');
-        }
-        else{
-            $btn.addClass('active');
-            $btn2.addClass('active');
-            $btn3.addClass('active');
-            $btn4.addClass('active');
-        }
+    if ($btn.hasClass('active')) {
+        $btn.removeClass('active');
+        $btn2.removeClass('active');
+        $btn3.removeClass('active');
+        $btn4.removeClass('active');
+    } else {
+        $btn.addClass('active');
+        $btn2.addClass('active');
+        $btn3.addClass('active');
+        $btn4.addClass('active');
+    }
 }
 
-function MobileSideBar__init(){
+function MobileSideBox__toggle(e) {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    }
+    else {
+        $(this).addClass('active');
+    }
+    e.stopPropagation();
+}
+
+function MobileSideBar__init() {
     $('.btn-toggle-mobile-side-bar,.background').click(MobileSideBar__toggle);
+    $('.side-box ul >li').click(MobileSideBox__toggle);
 }
 
-$(function(){
+$(function () {
     MobileSideBar__init();
 })
