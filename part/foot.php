b85d1f7264af03497b86bc249be51e164f0b9892
yangmikang <ymk0668@naver.com>
2020-07-07 18:44:54 +0900

    탑바 연결 detail.php1차

--- part/foot.php
+++ part/foot.php
@@ -1,2 +1,12 @@
+<div class="copyright">
+    <div class="text">
+        <a href="#">
+            (주)엠씨케이퍼블리싱 서울특별시 강남구 봉은사로 226 Tel 02.3458.7300 Fax 02.3458.7119 정기구독문의 Tel 02.3458.7122
+            사업자등록번호211-86-54814 통신판매업신고제 2007-3210121-30-2-10512호 대표 손기연
+            Copyright © 2017 MCK Publishing Co. Ltd. All Rights Reserved.
+        </a>
+    </div>
+</div>
+
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,203 @@
 <?php
 include "../part/head.php"
 ?>
+
+<div class="bn-slider-bar ">
+        <div class="bn-slider">
+            <div style="background-image:url(https://user-images.githubusercontent.com/66409890/86404279-44270b00-bcea-11ea-9c53-a5277be9eb16.png);"
+                class="active"></div>
+            <div
+                style="background-image:url(https://user-images.githubusercontent.com/66409890/86404308-4e490980-bcea-11ea-8393-b229d83490c1.png);">
+            </div>
+            <div style="background-image:url(./resource/img/배너1.png);"></div>
+        </div>
+        <div class="side-bars">
+            <div>
+                <span>
+                    <i class="fa fa-angle-left" aria-hidden="true"></i>
+                </span>
+            </div>
+            <div>
+                <span><i class="fa fa-angle-right" aria-hidden="true"></i></span>
+            </div>
+        </div>
+    </div>
+
+    <div class="about-me-logo flex flex-1-0-0 con-2">
+        <div class="text-logo">
+            <a href="#">
+                About ME
+            </a>
+        </div>
+    </div>
+    <div class="about con-2 flex">
+        <div class="profile flex">
+            <div class="profile-img">
+                <img src="./resource/img/프로필.png" alt="">
+            </div>
+        </div>
+
+        <div class="profile-text flex">
+            <ul>
+                <li>이름 : Yang Mi Kang</li>
+                <li>주소 : 대전시 서구 SBS아카데미</li>
+                <li>나이 : 23</li>
+                <li>소개글 : 자기를 소개하는 내용을 적은 모든 글이 포함될 수 있다. 모든 글이 포함될 수 있다. </li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="portfolio con-2">
+        <div class="portfolio-logo">
+            <a href="#">PortFolio</a>
+        </div>
+    </div>
+    <div class="portfolio-list con-2">
+        <ul class="flex flex-wrap">
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png"></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+            <li>
+                <div class="img-box"><img src="./resource/img/고양이.png" alt=""></div>
+            </li>
+        </ul>
+    </div>
+    <div class="update con-2">
+        <div class="update-logo">
+            <a href="#">Update</a>
+        </div>
+    </div>
+    <div class="update-list con-2">
+        <ul>
+            <li>
+                <div class="update-box">
+                    <div class="img-box2"><img src="./resource/img/흑백.png" alt=""></div>
+                    <div class="update-title"><a href="#">최신 글 제목</a></div>
+                    <div class="update-text"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                            maiores asperiores corporis,
+                            ea quam, molestias quaerat fugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipisci?</a>
+                    </div>
+                    <div class="date"><a href="#">2020.07.02 10:20</a></div>
+                </div>
+
+
+
+            </li>
+            <li>
+                <div class="update-box">
+                    <div class="img-box2"><img src="./resource/img/흑백.png" alt=""></div>
+                    <div class="update-title"><a href="#">최신 글 제목</a></div>
+                    <div class="update-text"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                            maiores asperiores corporis,
+                            ea quam, molestias quaerat fugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipisci?</a>
+                    </div>
+                    <div class="date"><a href="#">2020.07.02 10:20</a></div>
+                </div>
+            </li>
+            <li>
+                <div class="update-box">
+                    <div class="img-box2"><img src="./resource/img/흑백.png" alt=""></div>
+                    <div class="update-title"><a href="#">최신 글 제목</a></div>
+                    <div class="update-text"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                            maiores asperiores corporis,
+                            ea quam, molestias quaerat fugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipisci?</a>
+                    </div>
+                    <div class="date"><a href="#">2020.07.02 10:20</a></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="most-popular con-2">
+        <div class="most-popular-logo">
+            <a href="#"> Most Popular</a>
+        </div>
+
+    </div>
+    <div class="most-popular-list con-2">
+        <ul>
+            <li>
+                <div class="popular-box">
+                    <a href="#" class="img-box2" style="background-image:url(./resource/img/여자.png)"></a>
+                    <div class="popular-title"><a href="#">베스트 글 제목</a></div>
+                    <div class="popular-text"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                            maiores asperiores corporis qu... molestias quaerat fugit rerum modi, totam ipsa quod
+                            doloribus error nam dolore
+                            laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                            laudantium quidem adipisci?</a>
+                    </div>
+                </div>
+
+            </li>
+            <li>
+                <div class="popular-box">
+                    <a href="#" class="img-box2" style="background-image:url(./resource/img/여자.png)"></a>
+                    <div class="popular-title"><a href="#">베스트 글 제목</a></div>
+                    <div class="popular-text"><a href="#"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing
+                                elit. Non
+                                maiores asperiores corporis qu... molestias quaerat fugit rerum modi, totam ipsa quod
+                                doloribus error nam dolore
+                                laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                                laudantium quidem adipisci?</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="popular-box">
+                    <a href="#" class="img-box2" style="background-image:url(./resource/img/여자.png)"></a>
+                    <div class="popular-title"><a href="#">베스트 글 제목</a></div>
+                    <div class="popular-text"><a href="#"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing
+                                elit. Non
+                                maiores asperiores corporis qu... molestias quaerat fugit rerum modi, totam ipsa quod
+                                doloribus error nam dolore
+                                laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                                laudantium quidem adipisci?</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="popular-box">
+                    <a href="#" class="img-box2" style="background-image:url(./resource/img/여자.png)"></a>
+                    <div class="popular-title"><a href="#">베스트 글 제목</a></div>
+                    <div class="popular-text"><a href="#"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing
+                                elit. Non
+                                maiores asperiores corporis qu... molestias quaerat fugit rerum modi, totam ipsa quod
+                                doloribus error nam dolore
+                                laudantium quidem adipiscfugit rerum modi, totam ipsa quod doloribus error nam dolore
+                                laudantium quidem adipisci?</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+
+
+
 <?php
 include "../part/foot.php"
 ?>
