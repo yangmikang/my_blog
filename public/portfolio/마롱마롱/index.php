cd49f561061bf1f84a16dbe5a1a3497cbc363407
yangmikang <ymk0668@naver.com>
2020-07-09 18:46:01 +0900

    마롱마롱 2차 하단메뉴까지 완성

--- public/portfolio/마롱마롱/index.php
+++ public/portfolio/마롱마롱/index.php
@@ -7,48 +7,121 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>마롱마롱</title>
     <link rel="stylesheet" href="./index.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css"
-        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
-    <script src="/resource/common.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"">
+    <script src=" https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"> </script> <script
+        src="/resource/common.js">
+    </script>
 </head>
 
 <body>
-    <div class="top-bar con ">
-        <div class="logo">
-            <img src="http://marongmarong.com/images/logo-white.png" alt="">
-        </div>
-        <div class="menu-bar con ">
-            <div class="menu-box">
-                <ul>
-                    <li><a href="#" class=>ABOUT</a></li>
-                    <li><a href="#" class=>DESIGN WORK </a></li>
-                    <li><a href="#" class=>ILLUST WORK</a></li>
-                    <li><a href="#" class=>CONTACT</a></li>
-                    <li><a href="#"><i class="fas fa-shopping-cart"></i></a>
-                    </li>
+    <div class="home">
+        <div class="top">
+            <div class="top-bar con-2 ">
+                <div class="logo">
+                    <a href="#"><img src="http://marongmarong.com/images/logo-white.png" alt=""></a>
 
-                </ul>
+                </div>
+                <div class="menu-bar con-2 ">
+                    <div class="menu-box">
+                        <ul>
+                            <li><a href="#" class=>ABOUT</a></li>
+                            <li><a href="#" class=>DESIGN WORK </a></li>
+                            <li><a href="#" class=>ILLUST WORK</a></li>
+                            <li><a href="#" class=>CONTACT</a></li>
+                            <li><a href="#"><i class="fas fa-shopping-cart"></i></a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
 
-    </div>
-    <div class="bn">
-        <div class="bn-img"
-            style="background-image:url(https://github.com/yangmikang/img1/blob/master/pf/%EB%A7%88%EB%A1%B1%EB%A7%88%EB%A1%B1/main_image5.jpg?raw=true);">
+
         </div>
-        <div class="center ">
-            <div class="center-box">
-                <div class="center-img">
-                    <img src="https://github.com/yangmikang/img1/blob/master/pf/%EB%A7%88%EB%A1%B1%EB%A7%88%EB%A1%B1/text2.png?raw=true"
-                        alt="">
+        <div class="bn">
+            <div class="bn-img"
+                style="background-image:url(https://github.com/yangmikang/img1/blob/master/pf/%EB%A7%88%EB%A1%B1%EB%A7%88%EB%A1%B1/main_image5.jpg?raw=true);">
+            </div>
+            <div class="center ">
+                <div class="center-box">
+                    <div class="center-img">
+                        <img src="https://github.com/yangmikang/img1/blob/master/pf/%EB%A7%88%EB%A1%B1%EB%A7%88%EB%A1%B1/text2.png?raw=true"
+                            alt="">
+                        <div class="down-icon"><a href="#"><i class="fas fa-arrow-down"></i></a></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="middle-text">
+            <div class="middle con-2">
+                <div class="section">
+                    <div class="text">
+                        <div class="img"><img src="./resource/img/전구.PNG" alt=""></div>
+                        <div class="span"></div>
+                    </div>
+                    <div class="content-box">
+                        <div class="title">Logo & Identity Design</div>
+                        <div class="content-text">브랜드 및 비즈니스의 가치가 고객에게 잘 전달되도록 로고 및 기업(브랜드)의 아이덴티티를 디자인 제작합니다.</div>
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="text">
+                        <div class="img"><img src="./resource/img/노트북.PNG" alt=""></div>
+                        <div class="span"></div>
+                    </div>
+                    <div class="content-box">
+                        <div class="title">Logo & Identity Design</div>
+                        <div class="content-text">브랜드 및 비즈니스의 가치가 고객에게 잘 전달되도록 로고 및 기업(브랜드)의 아이덴티티를 디자인 제작합니다.</div>
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="text">
+                        <div class="img"><img src="./resource/img/붓.PNG" alt=""></div>
+                        <div class="span"></div>
+                    </div>
+                    <div class="content-box">
+                        <div class="title">Logo & Identity Design</div>
+                        <div class="content-text">브랜드 및 비즈니스의 가치가 고객에게 잘 전달되도록 로고 및 기업(브랜드)의 아이덴티티를 디자인 제작합니다.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="about">
+            <div class="container ">
+                <div class="card flex">
+                    <div class="card-img" style="background-image:url(https://github.com/yangmikang/img1/blob/master/pf/%EB%A7%88%EB%A1%B1%EB%A7%88%EB%A1%B1/main_businesscards3.jpg?raw=true);"></div>
+                    <div class="card-text">
+                        <div class="card-padding">
+                            <h5>
+                                <span>Graphic</span><span>Design Studio.</span>
+                            </h5>
+                            <div class="card-line"></div>
+                            <p>
+                            I believe that my design can boost your brand image, in the same way the Marong Marong Studio is growing.
+                            <br>
+                            <br>
+                            제 디자인으로 마롱마롱 스튜디오가 성장하고 있는 것과 같이 당신의 브랜드 및 비즈니스 이미지를 끌어올릴 수 있다고 믿습니다.
+                                
+                            </p>
+                        </div>
+                    </div>
+                    <div class="plant" style="background-image:url(https://github.com/yangmikang/img1/blob/master/pf/%EB%A7%88%EB%A1%B1%EB%A7%88%EB%A1%B1/main_plant.jpg?raw=true);"></div>
+                </div>
+            </div>
+        </div>
+        <div class="project">
+            <div class="project-menu">
+                <div class="project-menu-box">
+                    <ul class="flex">
+                        <li><a href="#">ALL</a></li>
+                        <li><a href="#">UI / UX</a></li>
+                        <li><a href="#">LOGO</a></li>
+                        <li><a href="#">PRINTED MATTER</a></li>
+                        <li><a href="#">ILLUSTRATION</a></li>
+                    </ul>     
+                </div>
 
-
-
+            </div>
+        </div>
 </body>
-
 </html>
