63e9e9aa0bb5f54ad3a39ae11546b25c17252346
yangmikang <ymk0668@naver.com>
2020-07-17 18:51:19 +0900

    최종

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/portfolio/마롱마롱">마롱마롱</a></li>
+        <li><a href="/portfolio/이솝">이솝</a></li>
+        <li><a href="https://yangmikang.github.io/img1/pf/artworks/1/강아지.png">강아지</a></li>
+
+
+    </ul>
+</body>
+</html>
--- public/portfolio/index.php
+++ public/portfolio/index.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+    <H1>코딩 작업물</H1>
     <ul>
-        <li><a href="/portfolio/마롱마롱">마롱마롱</a></li>
-        <li><a href="/portfolio/이솝">이솝</a></li>
+        <li><a href="/portfolio/마롱마롱">마롱마롱</a>
+            <ul>
+                <li><a href="http://marongmarong.com/home-design.php">마롱마롱 원본 사이트</a></li>
+            </ul>
+        </li>
+        <li><a href="/portfolio/이솝">이솝</a>
+            <ul>
+                <li><a
+                        href="https://www.aesop.com/kr/?gclid=CjwKCAjwmMX4BRAAEiwA-zM4JgbVgQEle24ZjN92AnLEnXjdSwfX4XZDiGw0j05pkYIpx81mAgv-ZBoCLLwQAvD_BwE">이솝
+                        원본 사이트</a></li>
+            </ul>
+        </li>
         <li><a href="/portfolio/artworks/1/강아지.png">강아지</a></li>
-
-
     </ul>
 </body>
+
 </html>
