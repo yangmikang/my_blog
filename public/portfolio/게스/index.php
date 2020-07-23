72007913d4fdc09635617bb8bb173704c5b1ab5c
yangmikang <ymk0668@naver.com>
2020-07-23 18:48:51 +0900

    게스 배너 완료

--- public/portfolio/게스/index.php
+++ public/portfolio/게스/index.php
@@ -1,25 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>게스</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- 아울 캐러셀 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
     <link rel="stylesheet" href="./index.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
-        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
     <script src="./index.js"></script>
 </head>
 
 <body>
-    <div class="g-top">
+    <div class="g-top">   
+         <div class="gs-login-bar flex -jc-end">
+                <div class="sp-bar ">
+                    <ul class="flex">
+                        <li class=""><i class="fas fa-search"></i><a href="#"></a></li>
+                        <li class=""><i class="far fa-user"></i><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
         <div class="g-main-bar flex">
             <div class="g-logo flex"><a href="https://www.guesskorea.com/main/main.php" target="_blank"><img
                         src="https://www.guesskorea.com/static/img/common/h1_logo.png" alt=""></a>
             </div>
-            <div class="g-menu-bar">
+            <div class="g-menu-bar flex">
                 <div class="g-menu-box">
                     <ul class="flex">
                         <li class=""><a href="#">WOMEN</a>
@@ -42,41 +56,38 @@
                         <li class="t-c"><a href="#">SUZY WIDE JEANS</a></li>
                     </ul>
                 </div>
-                <div class="sp-bar flex">
-                    <ul class="flex">
-                        <li class=""><a href="#"></a></li>
-                        <li class=""><a href="#"></a></li>
-                    </ul>
+
+
+            </div>
+            
+        </div>
+    
+
+        <div class="my-slider-1">
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <h4><img src="https://user-images.githubusercontent.com/66409890/88208664-8b227380-cc8c-11ea-8718-f107b850e657.jpg"
+                            alt=""></h4>
+                </div>
+                <div class="item">
+                    <h4><img src="https://user-images.githubusercontent.com/66409890/88210468-38968680-cc8f-11ea-9b2c-daf0b69d4ad0.jpg"
+                            alt=""></h4>
+                </div>
+                <div class="item">
+                    <h4><img src="https://user-images.githubusercontent.com/66409890/88210479-3d5b3a80-cc8f-11ea-9408-e32cb7e72310.jpg"
+                            alt=""></h4>
+                </div>
+                <div class="item">
+                    <h4><img src="https://user-images.githubusercontent.com/66409890/88210475-3b917700-cc8f-11ea-99b4-a5d294860f82.jpg"
+                            alt=""></h4>
+                </div>
+                <div class="item">
+                    <h3><img src="https://user-images.githubusercontent.com/66409890/88210486-3f24fe00-cc8f-11ea-9649-5a8cfe20e786.jpg"
+                            alt=""></h3>
                 </div>
             </div>
         </div>
-
-    </div>
-
-    <div class="bn-slider-bar bn-slider-bar-2">
-        <div class="bn-slider">
-            <div style="background-image:url(https://user-images.githubusercontent.com/66409890/88208664-8b227380-cc8c-11ea-8718-f107b850e657.jpg);"
-                class="active"></div>
-            <div
-                style="background-image:url(https://user-images.githubusercontent.com/66409890/88210475-3b917700-cc8f-11ea-99b4-a5d294860f82.jpg);">
-            </div>
-            <div style="background-image:url(https://user-images.githubusercontent.com/66409890/88210479-3d5b3a80-cc8f-11ea-9408-e32cb7e72310.jpg);"></div>
-            <div style="background-image:url(https://user-images.githubusercontent.com/66409890/88210486-3f24fe00-cc8f-11ea-9649-5a8cfe20e786.jpg);"></div>
-            <div style="background-image:url(https://user-images.githubusercontent.com/66409890/88210468-38968680-cc8f-11ea-9b2c-daf0b69d4ad0.jpg);"></div>
-        </div>
-        <div class="side-bars">
-            <div>
-                <span>
-                    <i class="fa fa-angle-left" aria-hidden="true"></i>
-                </span>
-            </div>
-            <div>
-                <span><i class="fa fa-angle-right" aria-hidden="true"></i></span>
-            </div>
-        </div>
-    </div>
-    
-    <script src="./index.js"></script>
+        <script src="./index.js"></script>
 </body>
 
 </html>
